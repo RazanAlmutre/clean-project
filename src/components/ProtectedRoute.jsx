a7cacdc626970086aad55cf8d46bdc2c2e554f86
Razan <almutire@MacBook-Air-Razan.local>
2026-06-22 17:20:22 +0300

    Update classes pages

--- src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const isAdmin = localStorage.getItem("admin") === "true";
-
-  return isAdmin ? children : <Navigate to="/" />;
-}
