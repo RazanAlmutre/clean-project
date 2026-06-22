8138a3cb9f312cad85b159a36023ffcb2d18a9a6
Razan <almutire@MacBook-Air-Razan.local>
2026-06-22 15:09:18 +0300

    fix vercel 404 routing

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://cxlthnmntpxmntckdqbx.supabase.co";
-const supabaseKey = "sb_publishable_tohZy6xPb3MfQkeWJDZNiQ_hYrxgzgi";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey
-);
+export const supabase = createClient(supabaseUrl, supabaseKey);
